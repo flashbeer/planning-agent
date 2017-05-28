f97918ea88b31cc95786f85ac8496f6a29b4aa48
Andres Root <info@andresroot.co>
2017-05-28 00:31:40 -0500

    Working on planning agent

--- problems.py
+++ problems.py
@@ -6,3 +6,9 @@ from aimacode.utils import expr
 from utils import (
     FluentState, encode_state, decode_state,
 )
+
+
+class BeerCargoProblem(Problem):
+
+    def __init__(self, cargos, trucks, warehouses, initial: FluentState, goal: list):
+        pass
